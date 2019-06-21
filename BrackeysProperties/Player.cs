fa4acfbec8104a55ad4003b0e4e1bc6753f9bd45
Stas Kolesnikov <SKolesnikov@Attentigroup.com>
2019-06-21 19:44:41 +0300

    Add project files.

---
+++ BrackeysProperties/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrackeysProperties
+{
+    class Player
+    {
+        private int _health = 100;
+        //Get only poroperty (Readonly access )
+        public int health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    _health = 0;
+                }
+                else if (value > 100)
+                {
+                    _health = 100;
+                }
+                else
+                {
+                    _health = value;
+
+                }
+            }
+        }
+    }
+}
+        //public void Damage(int _dmg)
+        //{
+        //    health -= _dmg;
+        //}
+    
+
---
+++ BrackeysProperties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrackeysProperties
+{
+    class MainCLass   
+    {
+        static void Main(string[] args)
+        {
+            Player tom = new Player();
+            Console.WriteLine(tom.health);
+
+            //tom.health =40;
+            //Console.WriteLine(tom.health);
+            //tom.Damage(50);
+            //tom.health = 30;  - can't be performed cause the "health" is readonly = only get
+
+            tom.health -= 200;
+            Console.WriteLine(tom.health);
+            tom.health += 400;
+            Console.WriteLine(tom.health);
+            tom.health -= 70;
+            Console.WriteLine(tom.health);
+
+
+        }
+    }
+}
